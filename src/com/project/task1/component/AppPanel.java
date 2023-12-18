8fd85f14a0432348762053466aea585f0a04c971
GeorgeBacalu <georgebacalu83@gmail.com>
2023-12-18 10:18:12 +0200

    Enforce arcs between nodes

--- src/com/project/task1/component/AppPanel.java
+++ src/com/project/task1/component/AppPanel.java
@@ -32,7 +32,16 @@ public class AppPanel extends JPanel {
                         nodes.add(new Node(event.getX(), event.getY(), nodeNumber++));
                     }
                 } else {
-                    arcs.add(new Arc(start, end));
+                    nodes.stream()
+                            .filter(node -> isPointOnNode(start, node))
+                            .findFirst()
+                            .ifPresent(startNode -> nodes.stream()
+                                    .filter(node -> isPointOnNode(end, node) && node != startNode)
+                                    .findFirst()
+                                    .ifPresent(endNode -> {
+                                        updateEnds(startNode, endNode);
+                                        arcs.add(new Arc(start, end));
+                                    }));
                 }
                 start = null;
                 isDragging = false;
@@ -54,6 +63,20 @@ public class AppPanel extends JPanel {
         return nodes.stream().noneMatch(node ->
                 event.getX() > node.getX() - minDistance && event.getX() < node.getX() + minDistance &&
                 event.getY() > node.getY() - minDistance && event.getY() < node.getY() + minDistance);
+    }
+
+    private boolean isPointOnNode(Point point, Node node) {
+        int radius = nodeDiameter / 2, centerX = node.getX() + radius, centerY = node.getY() + radius;
+        int distance = (int) Math.sqrt(Math.pow(point.x - centerX, 2) + Math.pow(point.y - centerY, 2));
+        return distance <= radius;
+    }
+
+    private void updateEnds(Node startNode, Node endNode) {
+        int radius = nodeDiameter / 2;
+        start.x = startNode.getX() + radius;
+        start.y = startNode.getY() + radius;
+        end.x = endNode.getX() + radius;
+        end.y = endNode.getY() + radius;
     }
 
     @Override
